ad28d3618b71f8e18c5899cf68267f76e3891698
Carol <anycarolinediasbarreto7@gmail.com>
2026-04-06 17:18:26 -0300

    criando o portifolio inicial

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+   
+    $("#btnTeste").click(function (){
+    //  $("#staticBackdrop").modal("show");
+    alert("Teste, botao clicado!");
+    });
+ 
+   //Abrir o modal automaticamente
+        $("#staticBackdrop").modal("show");
+   // });
+});
